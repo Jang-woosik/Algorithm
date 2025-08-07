6f2d1d19970e9ba692b70d8fc45e0d170b926bbd
Jang-woosik <jjys0418@naver.com>
2025-08-07 17:15:22 +0900

    [Silver I] Title: 음식물 피하기, Time: 92 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+
+/*
+ * 
+ * 2 3 3 4를 입력받아서 2 4가 연결됐는데
+
+ * 
+ */
+
+
+public class Main {
+	
+	static int N;
+	static int M;
+	static boolean[][] visit;
+	static int[][]arr;
+	static int cnt;
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		int K=Integer.parseInt(st.nextToken());
+		arr=new int[N+1][M+1];
+		visit=new boolean[N+1][M+1];
+		
+		
+		for(int i=0;i<K;i++) {
+			st=new StringTokenizer(br.readLine());
+			int a=Integer.parseInt(st.nextToken());
+			int b=Integer.parseInt(st.nextToken());
+			arr[a][b]=1;
+		}
+		int max=0;
+		
+		for(int i=1;i<N+1;i++) {
+			for(int j=1;j<M+1;j++) {
+				if(arr[i][j]==1&&!visit[i][j]) {
+					cnt=0;
+					dfs(i,j);
+					if(max<cnt) {
+						max=cnt;
+					}
+				}
+			}
+		}
+		System.out.println(max);
+		
+		
+	}
+	
+	public static void dfs(int x,int y) {
+		cnt++;
+		int[] dx= {1,-1,0,0};
+		int[]dy= {0,0,1,-1};
+		visit[x][y]=true;
+		
+		for(int i=0;i<4;i++) {
+			int nx=dx[i]+x;
+			int ny=dy[i]+y;
+			if(nx>=1&&ny>=1&&nx<N+1&&ny<M+1&&arr[nx][ny]==1&&!visit[nx][ny]) {
+				dfs(nx,ny);
+			}
+		}
+		
+	}
+}
