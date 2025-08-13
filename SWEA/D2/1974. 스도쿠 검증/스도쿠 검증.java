dc431e9a50c2b715d53beab335af3e2742265b97
Jang-woosik <jjys0418@naver.com>
2025-08-13 16:06:09 +0900

    [D2] Title: 스도쿠 검증, Time: 89 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,78 +1,93 @@
-import java.io.*;
-import java.util.*;
-
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /*
- * 가로세로 확인
- * 작은 네모칸 확인
- * contains를 통해서 포함됐는지
- * 
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
  */
+class Solution
+{
+	static int[][] grid = new int[9][9];
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for(int test_case = 1; test_case<=T; test_case++){
+            for(int str = 0; str<9; str++){
+                String[] input_line = br.readLine().split(" ");
+                for(int inside = 0; inside<9; inside++){
+                    grid[str][inside]=Integer.parseInt(input_line[inside]);
+                }
+            }
+            
+            bw.write("#"+test_case+" "+check());
+            bw.newLine();
+            bw.flush();
+        }
+        bw.close();
+        br.close();
+    }
 
-public class Solution {
-
-	
-	public static void main(String[] args)throws IOException{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int T=Integer.parseInt(br.readLine());
-		for(int s=0;s<T;s++) {
-			int[][]arr=new int[9][9];
-			boolean visit=false;
-			
-			for(int i=0;i<9;i++) {
-				StringTokenizer st=new StringTokenizer(br.readLine());
-				for(int j=0;j<9;j++) {
-					arr[i][j]=Integer.parseInt(st.nextToken());
-				}
-			}
-			//가로줄 확인
-			for(int i=0;i<9;i++) {
-				ArrayList<Integer>arr2=new ArrayList<>();
-				for(int j=0;j<9;j++) {
-					if(arr2.contains(arr[i][j])) {
-						visit=true;
-					}
-					arr2.add(arr[i][j]);
-				}
-			}
-			
-			//세로줄 확인
-			for(int i=0;i<9;i++) {
-				ArrayList<Integer>arr2=new ArrayList<>();
-				for(int j=0;j<9;j++) {
-					if(arr2.contains(arr[j][i])) {
-						visit=true;
-					}
-					arr2.add(arr[j][i]);
-				}
-			}
-			
-			
-			
-			//작은 사각형 확인
-			for(int start=0;start<8;start+=3) {
-				for(int fin=0;fin<8;fin+=3) {
-					ArrayList<Integer>arr2=new ArrayList<>();
-					for(int i=start;i<start+3;i++) {
-						
-					
-						for(int j=fin;j<fin+3;j++) {
-							if(arr2.contains(arr[i][j])) {	
-								visit=true;
-							}
-							arr2.add(arr[i][j]);
-						}
-					}
-				}	
-			}
-			if(visit) {
-				System.out.println("#"+(s+1)+" 0");
-			}else if(!visit) {
-				System.out.println("#"+(s+1)+" 1");
-			}
-			
-		}
-		
-	}
+    static int check(){
+        for(int i = 0; i<9; i++){
+            boolean[] inside_visited = new boolean[10];
+            boolean[] outside_visited = new boolean[10];
+            for(int j = 0; j<9; j++){
+                if(inside_visited[grid[i][j]]){
+                    return 0;
+                }
+                inside_visited[grid[i][j]] = true;
+                if(outside_visited[grid[j][i]]){
+                    return 0;
+                }
+                outside_visited[grid[j][i]] = true;
+            }
+        }
+        for (int i=0; i<9; i+=3){
+            for(int j = 0; j<9; j+=3){
+                boolean[] gridCheck = new boolean[10];
+                for(int gy=i; gy<i+3; gy++){
+                    for(int gx=j; gx<j+3; gx++){
+                        if(gridCheck[grid[gy][gx]]){
+                            return 0;
+                        }
+                        gridCheck[grid[gy][gx]]=true;
+                    }
+                }
+            }
+        }
+        return 1;
+    }
 }
