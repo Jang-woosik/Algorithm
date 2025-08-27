8c6528792996eb3445819f0af2d1674642b28bcc
Jang-woosik <jjys0418@naver.com>
2025-08-27 12:32:07 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,975 ms, Memory: 237,544 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,86 +2,84 @@ import java.io.*;
 import java.util.*;
 
 /*
- * 각 점에서 다른 점까지 가는데 거리 측정
- * 사이클이 생기면 최소 비용이 아니기때문에 union-find사용
- * 최소 비용이 언제인지부터 찾아서 시작
+ * 1에서 방문할 곳 큐에 넣고 2에서 방문할곳 큐에 넣고
+ * 우선순위 큐이기때문에 계속 반복하면 최소거리가 구해짐
+ * 방문 안 한곳만 계속 반복시키면 됨
  */
 
 
 public class Solution { 
 	
-	static class Test implements Comparable<Test>{
-		int from,to,m;
-		public Test(int from, int to, int m) {
-			this.from=from;
+	static class Node implements Comparable<Node>{
+		int to, cost;
+		public Node(int to, int cost) {
 			this.to=to;
-			this.m=m;
+			this.cost=cost;
 		}
-		
-		public int compareTo(Test t) {
-			return this.m-t.m;
+		public int compareTo(Node o) {
+			return this.cost-o.cost;
 		}
-		
 	}
 	
-	
-	static int[] parent;
+	static ArrayList<Node>[]list;
+	static int[]result;
+	static int[]parent;
+	static boolean[]visit;
+	static long total;
+	static int V;;
 	
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       
-       int t=Integer.parseInt(br.readLine());
-       
-       for(int s=1;s<t+1;s++) {
-    	   StringTokenizer st=new StringTokenizer(br.readLine());
-    	   int V=Integer.parseInt(st.nextToken());
-    	   int E=Integer.parseInt(st.nextToken());
-    	   parent=new int[V+1];
-    	   
-    	   for(int i=1;i<V+1;i++) {
-    		   parent[i]=i;
-    	   }
-    	   PriorityQueue<Test>q=new PriorityQueue<>();
-    	   
-    	   for(int i=0;i<E;i++) {
-    		   st=new StringTokenizer(br.readLine());
-    		   int from=Integer.parseInt(st.nextToken());
-    		   int to=Integer.parseInt(st.nextToken());
-    		   int cost=Integer.parseInt(st.nextToken());
-    		   q.add(new Test(from,to,cost));
-    	   }
-    	   
-    	   long ans=0;
-    	   int cost=0;
-    	   while(!q.isEmpty()) {
-    		   Test test=q.poll();
-    		   if(!union(test.from,test.to)) {
-    			   continue;
-    		   }
-    		   ans+=test.m;
-    		   if(++cost==V-1) {
-    			   break;
-    		   }
-    	   }
-    	   System.out.println("#"+s+" "+ans);
-       }
-       
-	}
-    
-    
-    
-    static int find(int x) {
-    	if(parent[x]==x)return x;
-    	return parent[x]=find(parent[x]);
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int T=Integer.parseInt(br.readLine());
+     for(int s=1;s<T+1;s++) {
+    	 StringTokenizer st=new StringTokenizer(br.readLine());
+         V=Integer.parseInt(st.nextToken());
+         int E=Integer.parseInt(st.nextToken());
+         list=new ArrayList[V+1];
+         result=new int[V+1];
+         parent=new int[V+1];
+         visit=new boolean[V+1];
+         
+         for(int i=1;i<V+1;i++) {
+       	  list[i]=new ArrayList<>();
+       	  result[i]=Integer.MAX_VALUE;
+         }
+         for(int i=0;i<E;i++) {
+       	  st=new StringTokenizer(br.readLine());
+       	  int a=Integer.parseInt(st.nextToken());
+       	  int b=Integer.parseInt(st.nextToken());
+       	  int c=Integer.parseInt(st.nextToken());
+       	  list[a].add(new Node(b,c));
+       	  list[b].add(new Node(a,c));
+         }
+         for(int i=1;i<V+1;i++) {
+       	  parent[i]=i;
+         }
+
+         System.out.println("#"+s+" "+dik(V));
+     }
     }
-    static boolean union(int x, int y) {
-    	int pa=find(x);
-    	int pi=find(y);
-    	if(pa!=pi) {
-    		parent[pi]=pa;
-    		return true;
+    static long dik(int end) {
+    	PriorityQueue<Node>q=new PriorityQueue<>();
+    	
+    	q.add(new Node(1,0));
+    	int cnt=0;
+    	total=0;
+    	
+    	while(!q.isEmpty()) {
+    		Node now=q.poll();
+    		if(!visit[now.to]) {
+    			cnt++;
+    			visit[now.to]=true;
+    			total+=now.cost;
+    			for(int i=0;i<list[now.to].size();i++) {
+    				q.add(list[now.to].get(i));
+    			}
+    		}
+    		if(cnt==end) {
+    			break;
+    		}
     	}
-    	return false;
+    	return total;
     }
-    
 }
